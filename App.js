3511371091fd7e1639514d47ae90686a09fcc698
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-09 23:41:38 +0530

    02 - 010 Improving The Layout

--- App.js
+++ App.js
@@ -8,8 +8,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals..</Text>
       </View>
     </View>
@@ -18,11 +17,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    /*මෙහිදී සම්පූර්ණ interface එකම එකම  flex (parent flex) එකක් ලෙස   appContainer  flex: 1 හරහා ගනී*/
+    /* අනතුරුව inputContainer(child flex) flex: 1  හා goalsContainer(child flex) flex: 5 හරහා*/
+    /*inputContainer එක 1/6 ක හා goalsContainer  එක 5/6 ක කොටසක් අත්පත් කර ගනී*/
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
@@ -31,5 +40,8 @@ const styles = StyleSheet.create({
     width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
