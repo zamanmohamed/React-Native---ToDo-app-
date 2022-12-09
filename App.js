41565c3ac5e15bdea394e6776c22ce8c99db4740
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-09 20:04:34 +0530

    02 - 04 inline style

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Button, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ padding: 20, borderColor: "red", borderWidth: 2 }}>
+        HI Mohamed Zaman
+      </Text>
+      <Text>Hello</Text>
+      <Button title="Tap me" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
