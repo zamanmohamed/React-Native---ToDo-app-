4f452e19a872911060dc00d701ae9101d9f25d25
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-10 02:00:24 +0530

    02 - 019 Handling Taps with the --Pressable-- Component

--- App.js
+++ App.js
@@ -14,6 +14,13 @@ export default function App() {
     ]);
   }
 
+  function deleteGoalHandler(id) {
+    // setCourseGoals((currentCourseGoals) => {
+    //   return currentCourseGoals.filter((goal) => goal.id !== id);
+    // });
+    console.log("DELETE GOAL..!");
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -21,7 +28,12 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
