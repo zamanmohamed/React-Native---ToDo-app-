b3c9279c1934aa07c49b4a75fd6e8829759de69d
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-09 20:20:15 +0530

    02 - 06

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Button, View } from "react-native";
+import { StyleSheet, Text, Button, View, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>HI Mohamed Zaman</Text>
-      <Text style={styles.dummyText}>Hello</Text>
-      <Button title="Tap me" />
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+
+      <View>
+        <Text>List of goals..</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: { padding: 20, borderColor: "red", borderWidth: 2, margin: 16 },
 });
