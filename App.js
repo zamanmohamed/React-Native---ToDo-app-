6b57dbd9228256c327b372638a50cb607111facd
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-10 02:14:08 +0530

    02 - 020 Making Items Deletable & Using IDs

--- App.js
+++ App.js
@@ -15,9 +15,9 @@ export default function App() {
   }
 
   function deleteGoalHandler(id) {
-    // setCourseGoals((currentCourseGoals) => {
-    //   return currentCourseGoals.filter((goal) => goal.id !== id);
-    // });
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
     console.log("DELETE GOAL..!");
   }
 
@@ -31,6 +31,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
